42880331392dfa0fa452cbaafe0effc59af79624
Olt Kryeziu <125915727+Oltkryeziu@users.noreply.github.com>
2024-12-01 07:52:40 -1100

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,165 @@
+const div = document.getElementById("input");
+const firstname_input = document.getElementById("firstname-input");
+const email_input = document.getElementById("email-input");
+const password_input = document.getElementById("password-input");
+const repeat_password_input = document.getElementById("repeat-password-input");
+const error_message = document.getElementById("error-message");
+
+form.addEventListener("submit", (e) => {
+  let errors = [];
+
+  if (firstname_input) {
+    errors = getSignupFormErrors(
+      firstname_input.value,
+      email_input.value,
+      password_input.value,
+      repeat_password_input.value
+    );
+  } else {
+    errors = getLoginFormErrors(
+      email_input.value,
+      password_input.value,
+      repeat_password_input.value
+    );
+  }
+
+  if (errors.length > 0) {
+    e.preventDefault();
+    error_message.innerText = errors.join(".");
+  }
+});
+
+function getSignupFormErrors(firstname, email, password, repeatPassword) {
+  let errors = [];
+
+  if (firstname === "" || firstname == null) {
+    errors.push("Firstname is required");
+    firstname_input.parentElement.classList.add("incorrect");
+  }
+  if (email === "" || email == null) {
+    errors.push("Email is required");
+    email_input.parentElement.classList.add("incorrect");
+  }
+  if (password === "" || password == null) {
+    errors.push("Password is required");
+    password_input.parentElement.classList.add("incorrect");
+  }
+  if (password.length < 8) {
+    errors.push("Password must have at least 8 characters");
+    password_input.parentElement.classList.add("incorrect");
+  }
+  if (password !== repeatPassword) {
+    errors.push("Password does not match repeated password");
+    password_input.parentElement.classList.add("incorrect");
+    repeat_password_input.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+}
+
+function getLoginFormErrors(email, password) {
+  let errors = [];
+
+  if (email === "" || email == null) {
+    errors.push("Email is required");
+    email_input.parentElement.classList.add("incorrect");
+  }
+  if (password === "" || password == null) {
+    errors.push("Password is required");
+    password_input.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+}
+
+const allInputs = [
+  FirstNameme,
+  email,
+  password,
+  repeat_password,
+].filter((input) => input != null);
+
+allInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.parentElement.classList.contains("incorrect")) {
+      input.parentElement.classList.remove("incorrect");
+      error_message.innerText = "";
+    }
+  });
+});
+
+//Prej qitu o sllajderi
+
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list");
+  const slideButtons = document.querySelectorAll(
+    ".slider-wrapper .slide-button"
+  );
+  const sliderScrollbar = document.querySelector(
+    ".container .slider-scrollbar"
+  );
+  const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+  // Handle scrollbar thumb drag
+  scrollbarThumb.addEventListener("mousedown", (e) => {
+    const startX = e.clientX;
+    const thumbPosition = scrollbarThumb.offsetLeft;
+    const maxThumbPosition =
+      sliderScrollbar.getBoundingClientRect().width -
+      scrollbarThumb.offsetWidth;
+
+    // Update thumb position on mouse move
+    const handleMouseMove = (e) => {
+      const deltaX = e.clientX - startX;
+      const newThumbPosition = thumbPosition + deltaX;
+      // Ensure the scrollbar thumb stays within bounds
+      const boundedPosition = Math.max(
+        0,
+        Math.min(maxThumbPosition, newThumbPosition)
+      );
+      const scrollPosition =
+        (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+      scrollbarThumb.style.left = `${boundedPosition}px`;
+      imageList.scrollLeft = scrollPosition;
+    };
+    // Remove event listeners on mouse up
+    const handleMouseUp = () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+    // Add event listeners for drag interaction
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  });
+  // Slide images according to the slide button clicks
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+  // Show or hide slide buttons based on scroll position
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+    slideButtons[1].style.display =
+      imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+  };
+  // Update scrollbar thumb position based on image scroll
+  const updateScrollThumbPosition = () => {
+    const scrollPosition = imageList.scrollLeft;
+    const thumbPosition =
+      (scrollPosition / maxScrollLeft) *
+      (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+    scrollbarThumb.style.left = `${thumbPosition}px`;
+  };
+  // Call these two functions when image list scrolls
+  imageList.addEventListener("scroll", () => {
+    updateScrollThumbPosition();
+    handleSlideButtons();
+  });
+};
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
